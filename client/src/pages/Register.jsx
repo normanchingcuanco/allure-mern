e4e9268c0f4cc31dc8956dbfe49e62231dcd6c12
normanchingcuanco <norman.chingcuanco@gmail.com>
2026-03-13 22:01:03 +0800

    remove node_modules and apply gitignore

--- client/src/pages/Register.jsx
+++
@@ -1,3 +0,0 @@
-export default function Discover() {
-  return <h1>Register Page</h1>
-}
--- server/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { createReport } from "../controllers/reportController.js"
-
-const router = express.Router()
-
-router.post("/", createReport)
-
-export default router
